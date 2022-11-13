a3b3eb5360b404286acb6a9955a640828f09d992
Suman <sumanjha@credenceanalytics.com>
2022-11-13 13:06:28 +0530

    Added and updated all the files

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const productRoutes = require("./routes/productRoutes");
+const connectDB = require("./config/db");
+const cors = require('cors');
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API running..." });
+});
+
+app.use("/api/products", productRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
